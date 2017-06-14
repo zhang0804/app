b391036cf58756c6a1ad40cde6e4e7f8604d90f3
zhangzhe1987 <619961945@qq.com>
2017-06-14 21:24:55 +0800

    zh

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 angular.module("App",['ionic','App.controllers'])
-.config(function($stateProvider,$urlRouterProvider){
+.config(['$stateProvider','$urlRouterProvider','$ionicConfigProvider',function($stateProvider,$urlRouterProvider,$ionicConfigProvider){
+    $ionicConfigProvider.platform.ios.tabs.style('standard');
+    $ionicConfigProvider.platform.ios.tabs.position('bottom');
+    $ionicConfigProvider.platform.android.tabs.style('standard');
+    $ionicConfigProvider.platform.android.tabs.position('standard');
+
+    $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+    $ionicConfigProvider.platform.android.navBar.alignTitle('center');
+    
+    $ionicConfigProvider.platform.ios.views.transition('ios');
+    $ionicConfigProvider.platform.android.views.transition('none');
+
     $stateProvider
         .state('tabs',{
             url:"/tabs",
@@ -13,7 +24,7 @@ angular.module("App",['ionic','App.controllers'])
                     templateUrl:'templates/first.html',
                     // controller:'firstctrl'
 
-    }
+                }
             }
         })
         .state('first-details',{
@@ -32,15 +43,15 @@ angular.module("App",['ionic','App.controllers'])
             templateUrl:'details/economics.html'
         })
         .state('tabs.Hotspot',{
-        url:"/Hotspot",
-        views:{
-            'Hotspot':{
-                templateUrl:'templates/Hotspot.html',
-                // controller:'Hotspotctrl'
+            url:"/Hotspot",
+            views:{
+                'Hotspot':{
+                    templateUrl:'templates/Hotspot.html',
+                    // controller:'Hotspotctrl'
 
+                }
             }
-        }
-    })
+        })
         .state('tabs.card',{
             url:"/card",
             views:{
@@ -67,12 +78,12 @@ angular.module("App",['ionic','App.controllers'])
                     // controller:'userctrl'
 
                 }
-        }
-    })
+            }
+        })
         .state('sign',{
             url:"/sign",
             templateUrl:'templates/sign.html'
         })
     $urlRouterProvider.otherwise('/sign');
 
-})
+}])
